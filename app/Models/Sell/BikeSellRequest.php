e2a8efa6e35af5e1430e1f6fca305ebb76ecca72
unknown <shohagm.iotait@gmail.com>
2023-05-23 12:57:08 +0600

    bike sell

--- app/Models/Sell/BikeSellRequest.php
+++ app/Models/Sell/BikeSellRequest.php
@@ -2,8 +2,13 @@
 
 namespace App\Models\Sell;
 
+use App\Models\Product\Brand;
+use App\Models\System\Area;
+use App\Models\System\City;
+use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BikeSellRequest extends BaseModel
 {
@@ -27,4 +32,42 @@ class BikeSellRequest extends BaseModel
         'created_at',
         'updated_at'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function area(): BelongsTo
+    {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function bike(): BelongsTo
+    {
+        return $this->belongsTo(SellBike::class);
+    }
+
+    public function getBikeImageAttribute($value)
+    {
+        $images = json_decode($value, true);
+        $html = '';
+
+        foreach ($images as $image) {
+            $image = asset('storage/'.$image);
+            $html .= "<img src='$image' alt='Image'>";
+        }
+
+        return $html;
+    }
 }
