f08285c2ca4ff112cbc89792b16891d2b544078b
MahmudIotaIT <mahmudhasan.iotait@gmail.com>
2023-05-29 11:53:03 +0600

    asset on buy now

--- Modules/Api/Http/Resources/Order/OrderResource.php
+++ Modules/Api/Http/Resources/Order/OrderResource.php
@@ -24,9 +24,9 @@ class OrderResource extends JsonResource
             'discount_rate' => $this->discount_rate,
             'price_after_discount' => $this->calculateDiscountPrice($this->price,$this->discount_rate),
             'total_stock' => $this->colors->first()->stock,
-            'image' => $this->image_url,
+            'image' => asset('storage/'.$this->image),
             'color' => $this->colors->first()->name,
-            'color_image' => $this->colors->first()->image_url,
+            'color_image' => asset('storage/'.$this->colors->first()->image),
         ];
     }
 }
