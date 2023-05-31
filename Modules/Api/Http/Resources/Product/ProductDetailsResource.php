f8f0bce382fa82956ee7d7e06801531424fcb28c
saifuliotait <saiful.iotait@gmail.com>
2023-05-31 12:22:19 +0600

    Update Product details

---
+++ Modules/Api/Http/Resources/Product/ProductDetailsResource.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Modules\Api\Http\Resources\Product;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Modules\Api\Http\Traits\Product\FeatureTrait;
+use Modules\Api\Http\Traits\Product\ProductTrait;
+
+class ProductDetailsResource extends JsonResource
+{
+    use ProductTrait;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'                   => $this->id,
+            'type'                 => $this->type,
+            'name'                 => $this->name,
+            'slug'                 => $this->slug,
+            'product_code'         => $this->product_code,
+            'image'                => asset('storage/' . $this->image_url),
+            'price'                => $this->price,
+            'discount_rate'        => $this->discount_rate,
+            'price_after_discount' => $this->calculateDiscountPrice($this->price, $this->discount_rate),
+            'in_stock'             => $this->total_stock,
+            'is_used'              => $this->is_used ?? 0,
+            'badge'                => $this->badge_url ? asset('storage/' . $this->badge_url) : "",
+            'brand'                => new BrandResource($this->brand),
+            'category'             => $this->category ? new CategoryResource($this->category) : [],
+            'colors'               => $this->colors ? ColorResource::collection($this->colors) : [],
+            'media'                => $this->media ? MediaResource::collection($this->media) : [],
+            'specifications'       => $this->specifications ? SpecificationResource::collection($this->specifications->where('is_key_feature', 0)) : [],
+            'summary'              => $this->specifications ? SpecificationResource::collection($this->specifications->where('is_key_feature', 1)) : [],
+            'description'          => $this->description,
+            'short_description'    => $this->short_description,
+            'is_favorite'          => $this->is_favorite,
+        ];
+    }
+}
