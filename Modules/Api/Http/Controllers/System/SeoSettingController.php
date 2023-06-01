1d5e09ec0dbd05ef1f2af499f7ab5809aeaea317
MahmudIotaIT <mahmudhasan.iotait@gmail.com>
2023-06-01 17:23:12 +0600

    seo resource

--- Modules/Api/Http/Controllers/System/SeoSettingController.php
+++ Modules/Api/Http/Controllers/System/SeoSettingController.php
@@ -5,13 +5,17 @@ namespace Modules\Api\Http\Controllers\System;
 use App\Http\Controllers\Controller;
 use App\Models\System\SeoSetting;
 use App\Models\System\SiteSetting;
+use Modules\Api\Http\Resources\System\SeoResource;
 
 class SeoSettingController extends Controller
 {
     public function seoSettings()
     {
+        $data = SeoSetting::select('page_title', 'page_description', 'page_keywords', 'page_url')
+            ->get();
+        //add this query into html meta tag
         return $this->respondWithSuccessWithData(
-            SeoSetting::select('page_title', 'page_description', 'page_keywords', 'page_url')
-                ->get());
+            SeoResource::collection($data)
+        );
     }
 }
