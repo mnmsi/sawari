37fffcfd6e9e14253c5a16804524ca3f6fff142c
unknown <shohagm.iotait@gmail.com>
2023-12-17 13:41:16 +0600

    banner images

--- app/Models/System/Banner.php
+++ app/Models/System/Banner.php
@@ -11,6 +11,8 @@ class Banner extends BaseModel
         'page',
         'show_on',
         'image_url',
+        'order_no',
+        'home_images',
         'is_active',
         'created_at',
         'updated_at',
@@ -19,4 +21,32 @@ class Banner extends BaseModel
     protected $casts = [
         'is_active' => 'boolean',
     ];
+
+    protected $appends = ['home_show_on'];
+
+    public function getHomeImageAttribute(): array
+    {
+        if (isset($this->attributes['id']) && !empty($this->attributes['home_images'])) {
+            $list = [];
+            $banner_list = json_decode($this->attributes['home_images'], true);
+            foreach ($banner_list as $l) {
+                $list[] = [
+                    "layout" => "video",
+                    "key" => rand(111111, 999999),
+                    "attributes" => [
+                        "home_image" => $l["image"],
+                        "image_url" => $l["url"],
+                    ]
+                ];
+            }
+            return $list;
+        } else {
+            return [];
+        }
+    }
+
+    public function getHomeShowOnAttribute()
+    {
+        return $this->attributes['show_on'];
+    }
 }
--- app/Nova/Banner.php
+++ app/Nova/Banner.php
@@ -4,12 +4,16 @@ namespace App\Nova;
 
 use App\Nova\Filters\BannerStatusFilter;
 use Illuminate\Http\Request;
+use Illuminate\Support\Env;
 use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\FormData;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Image;
 use Laravel\Nova\Fields\Number;
 use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\URL;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Whitecube\NovaFlexibleContent\Flexible;
 
 class Banner extends Resource
 {
@@ -42,12 +46,13 @@ class Banner extends Resource
      *
      * @param \Laravel\Nova\Http\Requests\NovaRequest $request
      * @return array
+     * @throws \Exception
      */
     public function fields(NovaRequest $request)
     {
         return [
             ID::make()->sortable(),
-//          page
+            //          page
             Select::make('Display Page', 'page')->options([
                 'home' => 'Home',
                 'all-bikes' => 'All Bike',
@@ -55,23 +60,88 @@ class Banner extends Resource
                 'bike-accessories' => 'Bike Accessories',
                 'our-showrooms' => 'Our Showrooms',
             ])->rules('required'),
-//            show on
+            //            show on type
             Select::make('Page Place', 'show_on')->options([
                 'all' => 'All',
                 'top' => 'Top',
                 'bottom' => 'Bottom',
-            ])->rules('required'),
-//            image
+            ])->dependsOn(['page'], function (Select $field, NovaRequest $request, FormData $formData) {
+                if (in_array($formData->page, ['all-bikes', 'popular-brands', 'bike-accessories', 'our-showrooms'])) {
+                    $field
+                        ->rules('required');
+                } else {
+                    $field
+                        ->hide()
+                        ->nullable();
+                }
+            }),
+            //            for home
+            //            show on type
+            Select::make('Home Banner Place', 'home_show_on')->options([
+                'top' => 'Top Slider',
+                'all' => 'All Banner',
+            ])->dependsOn(['page'], function (Select $field, NovaRequest $request, FormData $formData) {
+                if ($formData->page == 'home') {
+                    $field
+                        ->rules('required');
+                } else {
+                    $field
+                        ->hide()
+                        ->nullable();
+                }
+            }),
+            //            image
             Image::make('Image', 'image_url')
                 ->path('banner')
                 ->disk('public')
-                ->creationRules('required')
-                ->updateRules('nullable')
-                ->disableDownload(),
+                ->disableDownload()
+                ->dependsOn(['home_show_on'], function (Image $field, NovaRequest $request, FormData $formData) {
+                    if ($formData->home_show_on != "all") {
+                        $field
+                            ->creationRules('required')
+                            ->updateRules('nullable');
+                    } else {
+                        $field
+                            ->hide()
+                            ->nullable();
+                    }
+                }),
+            //            home page
+            Flexible::make('Banner Images', 'home_image')
+                ->dependsOn(['home_show_on', 'page'], function (Flexible $field, NovaRequest $request, FormData $formData) {
+                    if ($formData->home_show_on == "all") {
+                        $field
+                            ->rules('required');
+                    } else {
+                        $field
+                            ->hide()
+                            ->nullable();
+                    }
+                })
+                ->button('Add banner Images* (Max 3)')
+                ->addLayout('Select image', 'video', [
+                    //                    image
+                    Image::make('Image', 'home_image')
+                        ->path('banner')
+                        ->disk('public')
+                        ->help("*use respected ratio on view")
+                        ->creationRules('required')
+                        ->updateRules('nullable'),
+                    // url
+                    URL::make('Url', 'image_url')
+                        ->rules('required')
+                        ->withMeta([
+                            'extraAttributes' => [
+                                'placeholder' => 'Enter promotional url',
+                            ],
+                        ]),
+                ])->hideFromIndex()
+                ->hideFromDetail()
+                ->limit(3),
             //            order no
             Number::make('Banner Position', 'order_no')
                 ->rules('required'),
-//            status
+            //            status
             Select::make('Status', 'is_active')->options([
                 '1' => 'Yes',
                 '0' => 'No',
@@ -85,7 +155,7 @@ class Banner extends Resource
                 ->displayUsing(function ($v) {
                     return $v ? "Active" : "Inactive";
                 }),
-//            date
+            //            date
             DateTime::make('Created At', 'created_at')
                 ->hideFromIndex()
                 ->default(now())
@@ -144,5 +214,67 @@ class Banner extends Resource
     public function actions(NovaRequest $request)
     {
         return [];
+    }
+
+    protected static function fillFields(NovaRequest $request, $model, $fields)
+    {
+        if ($request->isCreateOrAttachRequest()) {
+            $fields = $fields->reject(function ($field) {
+                return ($field->attribute == 'home_image' || $field->attribute == 'home_show_on');
+            });
+        }
+
+        if ($request->isUpdateOrUpdateAttachedRequest()) {
+            $fields = $fields->reject(function ($field) {
+                return ($field->attribute == 'home_image' || $field->attribute == 'home_show_on');
+            });
+        }
+
+        return parent::fillFields($request, $model, $fields);
+    }
+
+    public static function afterCreate(NovaRequest $request, $model): void
+    {
+        $formData = $request->all();
+
+        if (isset($formData['home_image'])) {
+            $result = [];
+            foreach ($formData['home_image'] as $b) {
+                $result[] = [
+                    "url" => $b["attributes"]["image_url"],
+                    "image" => Env::get("APP_URL") . "storage/" . $request->all()[$b["attributes"]["home_image"]]->store("banner", "public"),
+                ];
+                $banner_item = \App\Models\System\Banner::find($model->id);
+                $banner_item->show_on = $formData['home_show_on'];
+                $banner_item->home_images = json_encode($result);
+                $banner_item->save();
+            }
+        }
+    }
+    public static function afterUpdate(NovaRequest $request, $model): void
+    {
+        $formData = $request->all();
+
+        if (isset($formData['home_image'])) {
+            $banner_item = \App\Models\System\Banner::find($model->id);
+            $result = json_decode($banner_item->home_images, true);
+            $update_data = [];
+            foreach ($formData['home_image'] as $k => $b) {
+                if (!empty($b["attributes"]["home_image"])) {
+                    $update_data[] = [
+                        "url" => $b["attributes"]["image_url"],
+                        "image" => Env::get("APP_URL") . "storage/" . $request->all()[$b["attributes"]["home_image"]]->store("banner", "public")
+                    ];
+                } else {
+                    $update_data[] = [
+                        "url" => $b["attributes"]["image_url"],
+                        "image" => $result[$k]["image"],
+                    ];
+                }
+            }
+            $banner_item->home_images = json_encode($update_data);
+            $banner_item->show_on = $formData['home_show_on'];
+            $banner_item->save();
+        }
     }
 }
